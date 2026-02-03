e44b333d37a66867e0cfb8cfdbdec0691be068a1
Konrad Czardybon <konradcz2001@gmail.com>
2026-02-03 13:37:32 +0100

    Chore: Bump application version to 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.konradcz2001"
-version = "1.2.1"
+version = "1.3.0"
 
 repositories {
     mavenCentral()
@@ -75,7 +75,7 @@ jlink {
     jpackage {
         imageName = "UpdateTracker"
         installerName = "UpdateTrackerSetup"
-        appVersion = "1.2.1"
+        appVersion = "1.3.0"
 
         installerType = "msi"
 
